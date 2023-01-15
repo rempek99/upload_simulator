1ac5fcd6769c9f60bb3dd1d8847a85487d6bbc69
rempek99 <arkadiusz.remplewicz@gmail.com>
2023-01-15 16:59:39 +0100

    Final

--- src/main/java/pl/zimnyciechan/uploadsimulator/service/QueueManager.java
+++ src/main/java/pl/zimnyciechan/uploadsimulator/service/QueueManager.java
@@ -39,8 +39,7 @@ public class QueueManager {
 
     private Client computeNext() {
         final int queueSize = clientSet.size();
-        final long maxTime = getMaxTime(clientSet);
-        Iterator<Client> iterator = clientSet.iterator();
+//        final long maxTime = getMaxTime(clientSet);
         Client winningClient = null;
         double maxPrio = 0.0;
         System.out.println("CLIENTS LEFT: " + queueSize);
@@ -48,23 +47,29 @@ public class QueueManager {
             long time = client.getDuration();
             Double size = client.getStorage().getFirstFile().getSize();
             double prio = 0;
-            if (client.getDuration() <= maxTime / size) {
-                prio = queueSize / size;
-//                prio = time * queueSize / size;
-                System.out.println("#1 group");
-            } else {
-//                prio = Math.pow(queueSize, time) * size;
-                prio = (double) time / queueSize;
-                System.out.println("#2 group");
-            }
+//TODO proba #2
+            prio = ((Math.pow(queueSize, 2)) * time) / size;
+
+//TODO proba #1
+//            if (client.getDuration() <= maxTime / size) {
+//                prio = queueSize / size;
+////                prio = time * queueSize / size;
+//                System.out.println("#1 group");
+//            } else {
+////                prio = Math.pow(queueSize, time) * size;
+//                prio = (double) time / queueSize;
+//                System.out.println("#2 group");
+//            }
             if (prio > maxPrio) {
                 maxPrio = prio;
                 winningClient = client;
             }
             System.out.println(client.getName() + " Prio: " + prio);
         }
-        System.out.println("Winner: " + winningClient.getName());
-        System.out.println("---------------------");
+        if (winningClient != null) {
+            System.out.println("Winner: " + winningClient.getName());
+            System.out.println("---------------------");
+        }
         return winningClient;
     }
 
